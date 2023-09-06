77061dc24901d98fab3c6950dd2a1bfbe87070e6
thuphan27379 <131003628+thuphan27379@users.noreply.github.com>
2023-09-06 11:32:56 +0700

    fix error not found

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Main from "./components/Main";
+import "./App.css";
+function App() {
+  return <Main />;
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import Result from "./Result"; //calculate
+import Display from "./Display"; //show result
+import Choices from "./Choices"; //user choice
+
+import { getRandomGameItem, calculatorUserWinner } from "../utils";
+
+const gameItems = [
+  //list item: rock, paper, scrissor
+  {
+    url: "/images/paper.png",
+    id: 0,
+    winItemIds: [1],
+    name: "Paper",
+  },
+  {
+    url: "/images/rock.png",
+    id: 1,
+    winItemIds: [2],
+    name: "Rock",
+  },
+  {
+    url: "/images/scissor.png",
+    id: 2,
+    winItemIds: [0],
+    name: "Scissor",
+  },
+];
+
+export default function Main() {
+  const [result, setResult] = useState("N/N");
+  const [userGameItem, setUserGameItem] = useState(null);
+  const [computerGameItem, setComputerGameItem] = useState(null);
+
+  const handleGameItemChange = (gameItem) => {
+    setUserGameItem({ ...gameItem });
+  };
+
+  useEffect(() => {
+    console.log("render");
+    if (userGameItem) {
+      const computerNewItem = getRandomGameItem(gameItems);
+      setComputerGameItem({ ...computerNewItem });
+      setResult(calculatorUserWinner(userGameItem, computerNewItem));
+    }
+  }, [userGameItem]);
+
+  return (
+    <div className="conainer">
+      <div className="main">
+        {/*  */}
+        <Result
+          user1GameItem={userGameItem}
+          user2GameItem={computerGameItem}
+          result={result}
+        />
+        {/* show result */}
+        <Display />
+        {/* user choice */}
+        <Choices
+          gameItems={gameItems}
+          handleGameItemChange={handleGameItemChange}
+        />
+      </div>
+    </div>
+  );
+}
